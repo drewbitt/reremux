0700a3274f78896976dc8ba8200898dc007e7b40
Andrew <andrew@drewbitt.com>
2018-08-14 02:37:28 -0500

    More merge work

--- merge.py
+++ merge.py
@@ -5,6 +5,7 @@ import sys
 import re
 from collections import defaultdict
 
+
 def mux(short_name, dest):
     """Mux a specified short names files in destination directory using mkvmerge"""
 
@@ -37,3 +38,56 @@ def mux(short_name, dest):
 
         # add to dict
         split_dict[episode_num].append(file)
+
+    # Dict for each episode is created. Loop through and do mkvmerge commmands
+
+    # Remember key is three digits with padded zeros. Check and see if three is really needed or just two
+    pad_with_three = False
+    for key in split_dict:
+        if int(key.lstrip("0")) > 99:
+            pad_with_three = True
+            break
+
+    # TODO: No idea how this implementation will work for more than 2 sub tracks in a language, like if there's
+    # an additional commentary sub track. Right now it will sort them by size fine, but won't be able to name
+    # them correctly since I don't know which is dialogue/commentary/whatever
+
+    # Actually this a huge fucking oversight - I'm never planning on specifying subtitle track names
+
+    print("For anime, auto-assign signs&songs and dialogue? (y/n)")
+    check_signs_songs = input() == "y"
+
+    for key, value in split_dict.items():
+        if not pad_with_three:
+            # pad with two instead of three. Yes, I know this is dumb, why even pad with three before?
+            # cuz didn't write it to only pad with two before UNLESS it needed three in demux, always three instead
+            key = (key.lstrip("0")).rjust(2, "0")
+
+        sub_files = [item for item in value if item.startswith("sub")]
+
+        # separate sub files based on language
+        sub_files_lang_dict = defaultdict(list)
+
+        for sub in sub_files:
+            pattern = re.compile(".*([a-z]{2})\.")
+            sub_country = pattern.match(sub).group(1)
+            sub_files_lang_dict[sub_country].append(sub)
+
+        print("Sub files lang dict: {}".format(sub_files_lang_dict))
+
+        # track 5 smaller one
+        if check_signs_songs:
+            for key, value in sub_files_lang_dict:
+                print(value)
+                if len(value) >= 2:
+                    filepaths = []
+                    for subf in value:
+                        filepaths.append(subf, os.path.getsize(subf))
+                    # smallest in the front
+                    filepaths.sort(key=lambda filename: filename[1], reverse=False)
+                    value = filepaths
+            print("After adjusting for signs and songs {}".format(sub_files_lang_dict))
+
+        # TODO: Always making English default right now. Ask for this
+        # TODO: Also I guess ask for subtitle titles if not signs and songs
+        mkvmerge_string = ""
