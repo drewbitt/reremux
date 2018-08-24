d05ac21d440c227d3b98c42628bed41a127e17cf
Andrew <andrew@drewbitt.com>
2018-08-24 15:22:14 -0400

    Add dict passing to rename

--- extras/rename.py
+++ extras/rename.py
@@ -5,50 +5,51 @@ import re
 import sys
 from shutil import move
 
-parser = ArgumentParser("Change episode numbers of already muxed files")
 
-parser.add_argument('dest', metavar="dest", type=str, help="Folder to look for files to rename")
-parser.add_argument("--file", dest="file", help="File location for file that specifies how to rename")
-args = parser.parse_args()
-# file format is
-# 01 -> 10
-# 10 -> 01
-# etc, one renaming on each line split by ->
+def rename(dest, file = False, dic=False):
+    # Rename files matching a regex as specified in either a file converted to a dict or a passed dict
 
-# Definitions
+    '''
+    # file format is
+    # 01 -> 10
+    # 10 -> 01
+    # etc, one renaming on each line split by ->
+    '''
 
-dest = os.path.normpath(args.dest)
-# pattern to get episode number
-pattern_ep_number = re.compile(r".* - ([0-9]+) \[.*")
+    # Definitions/Helper methods
+    dest = os.path.normpath(args.dest)
 
-def calc_rename(str1, new_ep_num):
-    return re.sub(r"(.* -)( [0-9]+ )(\[.*)", r"\1 {} \3".format(new_ep_num), str1)
+    def calc_rename(str1, new_ep_num):
+        # Calculate new string to rename file as
+        return re.sub(r"(.* -)( [0-9]+ )(\[.*)", r"\1 {} \3".format(new_ep_num), str1)
 
-def find_file(episode_num, z1=False):
-    if z1:
+    def find_file(episode_num, z1=False):
+        # Find file with passed episode number in dest directory
         matching_regex = re.compile(r".*- {} \[.*".format(episode_num))
-        if len([matching_regex.match(i) for i in os.listdir(dest) if matching_regex.match(i)]) > 1:
+        if z1 and len([matching_regex.match(i) for i in os.listdir(dest) if matching_regex.match(i)]) > 1:
             # found a z1 for the episode number
             matching_regex = re.compile(r".*- {} \[.*z1$".format(episode_num))
+
+        return "".join([matching_regex.match(i).group(0) for i in os.listdir(dest) if matching_regex.match(i)])
+
+    def to_d(vr):
+        # add dest to path
+        return os.path.join(dest, vr)
+
+    # Loops
+    if not dic:
+        with open(to_d(file)) as list_file:
+            episode_map = {}
+
+            # iterate over list file and add to episode_map
+            for line in list_file:
+                line = line.replace(" ", "").rstrip()
+                if line != "":
+                    orig_ep_num, new_ep_num = line.split("->")
+                    episode_map[orig_ep_num] = new_ep_num
     else:
-        matching_regex = re.compile(r".*- {} \[.*".format(episode_num))
-
-    return "".join([matching_regex.match(i).group(0) for i in os.listdir(dest) if matching_regex.match(i)])
-
-def to_d(vr):
-    # add dest to path
-    return os.path.join(dest, vr)
-
-# Loops
-with open(args.file) as list_file:
-    episode_map = {}
-
-    # iterate over list file and add to episode_map
-    for line in list_file:
-        line = line.replace(" ", "").rstrip()
-        if line != "":
-            orig_ep_num, new_ep_num = line.split("->")
-            episode_map[orig_ep_num] = new_ep_num
+        # expects dict like {"04": "03", "03": "04"}
+        episode_map = dic
 
     for count, key in enumerate(episode_map):
         # search only things I haven't processed yet
@@ -82,3 +83,17 @@ with open(args.file) as list_file:
             new_name = calc_rename(found_file, episode_map[key]).rstrip("z1")
             print("Moving {0} to {1}".format(found_file, new_name))
             move(to_d(found_file), to_d(new_name))
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser("Change episode numbers of already muxed files")
+
+    parser.add_argument('dest', metavar="dest", type=str, help="Folder to look for files to rename")
+    parser.add_argument("--file", dest="file", help="File location for file that specifies how to rename")
+    args = parser.parse_args()
+
+    if not args.file:
+        print("Please specify a file (--file) to read the renaming layout from")
+        sys.exit(1)
+
+    rename(args.dest, file=args.file)
