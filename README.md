c745dca2eb85b3db076067d6876a26420066787d
Andrew Bitterling <andrew@drewbitt.com>
2018-08-24 00:54:22 -0400

    super broken rename, going to fix ASAP

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # reremux
 
-Remux script meant to run on Linux, so eac3to in wine and mkvmerge. 
+Remux script meant to run on Linux, so eac3to in wine and mkvmerge.
 Demuxes with eac3to to filenames that contains all information needed to later mux together.
 
 Very specific to my use-case, so there's going to be some issues/to-dos, namely:
+
 * Can't name subtitle tracks
 * Can't name any track actually, I never prompt for it
 * If eac3to can even do 4K, it still will name the files with h264 since I didn't put h264/h265 in the filename to mux with
 * Has English audio always as default
 * Has very limited testing so surely things will be broken
-* ``--mux-only` actually still requires a source AND it to be like `source dest --mux-only`.
+* `--mux-only` actually still requires a source AND it to be like `source dest --mux-only`.
 * Error catching or notifying is bad. I can't help this easily since eac3to usually returns non-zero return codes for errors but does not in wine. An alternative is to parse for known error strings.
--- extras/rename.py
+++ extras/rename.py
@@ -2,6 +2,7 @@
 from argparse import ArgumentParser
 import os
 import re
+import sys
 from shutil import move
 
 parser = ArgumentParser("Change episode numbers of already muxed files")
@@ -32,28 +33,40 @@ with open(args.file) as list_file:
         file_ep_num_re = pattern.match(file1)
 
         if file_ep_num_re is not None:
-            print("\n" + file1 + "\n")
             file_ep_num = file_ep_num_re.group(1)
+            print(file_ep_num)
             if file_ep_num in episode_map:
-                # this is the file I need to rename
+                # rename file_ep_num to episode_map[file_ep_num]
 
                 def calc_rename(str1, new_ep_num):
-                    return re.sub(r"(.* - )([0-9]+)( \[.*)", r"\1 {} \3".format(new_ep_num), str1)
+                    return re.sub(r"(.* -)( [0-9]+ )(\[.*)", r"\1 {} \3".format(new_ep_num), str1)
 
                 # check and see if it can be a ez rename
-                if not new_ep_num in episode_map:
-                    print("ez rename")
-                    new_name = calc_rename(file1, new_ep_num)
+                if not file_ep_num in episode_map:
+                    new_name = calc_rename(file1, new_ep_num).rstrip("z1")
                     print("Moving {0} to {1}".format(file1, new_name))
                     # move(file1, new_name)
                 else:
-                    print("not ez rename")
-                    # rename the file to be this now
-                    new_file1 = file1 + "z1"
+                    # Means there's en existing file with this episode number
+                    # Find it in the directory
+                    matching_regex = re.compile(r".*- {} \[.*(?<!z1)$".format(episode_map[file_ep_num]))
+                    matching_file_move_to = "".join([matching_regex.match(i).group(0) for i in os.listdir(dest) if matching_regex.match(i)])
 
-                    #move(file1, new_file1)
-                    print("Renaming {0} to {1}".format(file1, new_file1))
+                    print(episode_map)
+                    print(episode_map[file_ep_num])
 
-                    new_name = calc_rename(new_file1, new_ep_num)
 
-                    print("Moving {0} to {1}".format(new_file1, new_name))
+                    if matching_file_move_to:
+                        print("Renaming {0} to {1}".format(matching_file_move_to, matching_file_move_to + "z1"))
+                        move(os.path.join(dest, matching_file_move_to), os.path.join(dest, matching_file_move_to + "z1"))
+
+                        new_name = calc_rename(matching_file_move_to, episode_map[file_ep_num])
+
+                        print("Moving {0} to {1}".format(file1, new_name))
+                        move(os.path.join(dest, file1), os.path.join(dest, new_name))
+                    else:
+                        print("Error")
+                        sys.exit(1)
+
+                # remove from episode map now that processed it
+                del episode_map[file_ep_num]
