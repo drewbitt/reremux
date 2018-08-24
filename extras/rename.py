2a1717cc07e5fb1026bcb144f94ce44579e81cf3
Andrew Bitterling <andrew@drewbitt.com>
2018-08-24 13:57:44 -0400

    Initial modifications of rename

--- extras/rename.py
+++ extras/rename.py
@@ -15,58 +15,63 @@ args = parser.parse_args()
 # 10 -> 01
 # etc, one renaming on each line split by ->
 
+# Definitions
+
 dest = os.path.normpath(args.dest)
+# pattern to get episode number
+pattern_ep_number = re.compile(r".* - ([0-9]+) \[.*")
 
-pattern = re.compile(r".* - ([0-9]+) \[.*")
+def calc_rename(str1, new_ep_num):
+    return re.sub(r"(.* -)( [0-9]+ )(\[.*)", r"\1 {} \3".format(new_ep_num), str1)
 
-all_files = os.listdir(dest)
+def find_file(episode_num, z1=False):
+    if z1:
+        matching_regex = re.compile(r".*- {} \[.*".format(episode_num))
+        if len([matching_regex.match(i) for i in os.listdir(dest) if matching_regex.match(i)]) > 1:
+            # found a z1 for the episode number
+            matching_regex = re.compile(r".*- {} \[.*z1$".format(episode_num))
+    else:
+        matching_regex = re.compile(r".*- {} \[.*".format(episode_num))
 
+    return "".join([matching_regex.match(i).group(0) for i in os.listdir(dest) if matching_regex.match(i)])
+
+# Loops
 with open(args.file) as list_file:
     episode_map = {}
+
+    # iterate over list file and add to episode_map
     for line in list_file:
         line = line.replace(" ", "").rstrip()
         if line != "":
             orig_ep_num, new_ep_num = line.split("->")
             episode_map[orig_ep_num] = new_ep_num
 
-    for num, file1 in enumerate(all_files):
-        file_ep_num_re = pattern.match(file1)
+    for count, key in enumerate(episode_map):
+        # search only things I haven't processed yet
+        # either do this or edit episode_map on the fly to delete seen elements
+        if episode_map[key] in dict(list(episode_map.items())[count:]):
+            # Not an easy rename - found another file that was specified to exist already
+            # in the list file
 
-        if file_ep_num_re is not None:
-            file_ep_num = file_ep_num_re.group(1)
-            print(file_ep_num)
-            if file_ep_num in episode_map:
-                # rename file_ep_num to episode_map[file_ep_num]
+            # rename episode of episode_map[key] to have "z1" at the end
+            # first, have to find it
+            rename_found_file = find_file(episode_map[key])
+            # filename of episode I need to rename
+            first_found_file = find_file(key)
 
-                def calc_rename(str1, new_ep_num):
-                    return re.sub(r"(.* -)( [0-9]+ )(\[.*)", r"\1 {} \3".format(new_ep_num), str1)
+            if not rename_found_file:
+                print("Did not find an episode for {}".format(episode_map[key]))
+                sys.exit(1)
+            if not first_found_file:
+                print("Did not find en episode for {}".format(key))
+                sys.exit(1)
 
-                # check and see if it can be a ez rename
-                if not file_ep_num in episode_map:
-                    new_name = calc_rename(file1, new_ep_num).rstrip("z1")
-                    print("Moving {0} to {1}".format(file1, new_name))
-                    # move(file1, new_name)
-                else:
-                    # Means there's en existing file with this episode number
-                    # Find it in the directory
-                    matching_regex = re.compile(r".*- {} \[.*(?<!z1)$".format(episode_map[file_ep_num]))
-                    matching_file_move_to = "".join([matching_regex.match(i).group(0) for i in os.listdir(dest) if matching_regex.match(i)])
-
-                    print(episode_map)
-                    print(episode_map[file_ep_num])
-
-
-                    if matching_file_move_to:
-                        print("Renaming {0} to {1}".format(matching_file_move_to, matching_file_move_to + "z1"))
-                        move(os.path.join(dest, matching_file_move_to), os.path.join(dest, matching_file_move_to + "z1"))
-
-                        new_name = calc_rename(matching_file_move_to, episode_map[file_ep_num])
-
-                        print("Moving {0} to {1}".format(file1, new_name))
-                        move(os.path.join(dest, file1), os.path.join(dest, new_name))
-                    else:
-                        print("Error")
-                        sys.exit(1)
-
-                # remove from episode map now that processed it
-                del episode_map[file_ep_num]
+            print("Renaming {0} to {1}".format(rename_found_file, rename_found_file + "z1"))
+            print("Moving {0} to {1}".format(first_found_file, rename_found_file))
+        else:
+            # Easier rename - the one I'm renaming to doesn't exist already
+            # prioritize if it ends in "z1"
+            found_file = find_file(key, z1=True)
+            # compute new name
+            new_name = calc_rename(found_file, episode_map[key]).rstrip("z1")
+            print("Moving {0} to {1}".format(found_file, new_name))
