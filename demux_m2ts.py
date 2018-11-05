d14f94ce0a4d7ceaf260e294b728e4ffa9eefe78
Andrew <andrew@drewbitt.com>
2018-11-04 20:38:46 -0500

    Gets audio language from playlist & rename now when demuxing m2ts

--- demux_m2ts.py
+++ demux_m2ts.py
@@ -2,9 +2,11 @@ import os
 import re
 import subprocess
 import sys
+from shutil import move
 
 import numpy
 
+import country_list
 import demux
 import split_video
 
@@ -24,10 +26,15 @@ def demux_m2ts(stdout, source, dest, oldcdw, start_num, pcm_to_flac_ans, twoch_t
     # If they were, they'll be removed. If not removed, they'd mess up episode numbering anyway
     m2ts_arr_paths_lengths = remove_outliers(m2ts_arr_paths_lengths)
 
-    # demux.demux_loop(eac3to_cmd, "", dest, start_num, m2ts_arr_paths_lengths, twoch_to_flac_ans, short_name,
-    #                 pcm_to_flac_ans, name_chapters, m2ts=True)
+    demux.demux_loop(eac3to_cmd, "", dest, start_num, m2ts_arr_paths_lengths, twoch_to_flac_ans, short_name,
+                     pcm_to_flac_ans, name_chapters, m2ts=True)
 
-    do_chapters(eac3to_cmd, source, dest, short_name, start_num, name_chapters, m2ts_arr_paths_lengths)
+    playlist_output = first_playlist_string(eac3to_cmd, source, dest)
+    found_chapters = do_chapters(eac3to_cmd, source, dest, short_name, start_num, name_chapters, m2ts_arr_paths_lengths,
+                                 playlist_output)
+
+    # Replace audio languages in files with audio language of munknown
+    replace_languages(playlist_output, short_name, found_chapters)
 
     # lazy hack for eac3to path issue
     os.chdir(oldcdw)
@@ -48,8 +55,9 @@ def get_m2ts_order(strd):
 
 def remove_outliers(arr):
     """ Removes outliers by comparing m2ts lengths """
-
     # arr from demux_m2ts is [(length, fullpath), (length, fullpath)] etc
+
+    print("Removing outliers")
     new_arr = numpy.empty(len(arr))
     for count, ele in enumerate(arr):
         new_arr[count] = ele[0]
@@ -65,15 +73,10 @@ def remove_outliers(arr):
     return final_list
 
 
-def do_chapters(eac3to_cmd, source, dest, short_name, start_num, name_chapters, m2ts_arr_paths_lengths):
-    """ Since chapters are only in the playlist, special treatment """
+def do_chapters(eac3to_cmd, source, dest, short_name, start_num, name_chapters, m2ts_arr_paths_lengths, str_output):
+    """ Since chapters are only in the playlist, special treatment. Returns true if found chapters, false if not"""
 
-    print("\nGetting chapter file from main playlist")
-    # Get info of first playlist
-    cmd = eac3to_cmd + " \"" + os.path.relpath(source,
-                                               start=dest) + "\" \"1)\" 2>/dev/null | tr -cd \"\\11\\12\\15\\40-\\176\""
-    proc = demux.run_shell(cmd, stdout1=subprocess.PIPE)
-    str_output = proc.stdout.decode()
+    print("Extracting chapter file from first playlist\n")
 
     # Run regex to get track number of the chapters (usually track 1)
     pattern = re.compile(r"([1-9])+: Chapters")
@@ -81,8 +84,8 @@ def do_chapters(eac3to_cmd, source, dest, short_name, start_num, name_chapters, 
     if match:
         chap_track = match.group(1)
     else:
-        print("Dod not find chapters")
-        return 2  # something
+        print("Did not find chapters")
+        return False
 
     # Get command ready to execute
     chap_name = "all_chapters" + "_" + short_name + ".txt"
@@ -94,6 +97,7 @@ def do_chapters(eac3to_cmd, source, dest, short_name, start_num, name_chapters, 
     demux.run_shell(eac3to_cmd_execute, stderr1=None)
 
     print("\nSplitting chapter file based on video times")
+
     # TODO: Might want to check if its on disk first
     # TODO: I am looking at m2ts times to do this. I already demuxed to h264, might use that
     # TODO: By calling split_by_video, I am getting m2ts times a second time, when I already did that
@@ -107,4 +111,53 @@ def do_chapters(eac3to_cmd, source, dest, short_name, start_num, name_chapters, 
     else:
         split_video.split_by_video(chap_name, False, paths, offset=int(start_num), file_name_format=n_format)
 
-    # TODO: Format of chapter files needs to be changed, chapter numbers only two digits, need to be three
+    return True
+
+
+def first_playlist_string(eac3to_cmd, source, dest):
+    print("\nRunning command for info from first playlist...")
+    cmd = eac3to_cmd + " \"" + os.path.relpath(source,
+                                               start=dest) + "\" \"1)\" 2>/dev/null | tr -cd \"\\11\\12\\15\\40-\\176\""
+    proc = demux.run_shell(cmd, stdout1=subprocess.PIPE)
+    return proc.stdout.decode()
+
+
+def replace_languages(str_output, short_name, found_chapters):
+    """ Replaces unknown audio/sub languages, since m2ts files often don't have language info, just playlists.
+     This currently does not work for audio/subs not present in the playlist, like one m2ts containing commentary
+     but playlist eac3to output doesnt have that listed"""
+
+    # Get muknown files
+    search_str = ("(sub|aud)_{}_.*track([0-9]+).*munknown.*$").format(short_name)
+    file_pattern = re.compile(search_str)
+    list_of_files = list(filter(file_pattern.match, os.listdir(os.getcwd())))
+
+    # Create dictionary from playlist info
+    playlist_pattern = re.compile(r"([1-9])+: (.*?), ([a-zA-Z]+)")  # may need to add additional unicode letters like å
+    match = playlist_pattern.findall(str_output)
+    track_lang_dict = dict()
+    for d in match:
+        track_lang_dict[d[0]] = (d[1], d[2])
+    print(track_lang_dict)
+
+    # PROBLEM: since mpls has chapters, 1 extra track, so track numbers will be off. Check if chapters and adjust when needed.
+
+    print("Adding languages to subtitle and audio track filenames")
+    print(list_of_files)
+    for i in list_of_files:
+        f = file_pattern.search(i)
+
+        type = f.group(1)
+        track_num = f.group(2)
+        if found_chapters:
+            track_num = str(int(track_num) + 1)
+        dict_info = track_lang_dict[track_num]
+
+        if (type == "sub" and "Subtitle" in dict_info[0]) or (type != "sub" and not ("Subtitle" in dict_info[0])):
+            # doing the != since aud could be truehd, pgs, dts, etc and I didnt wanna list out rn
+
+            country_code = [item[0] for item in country_list.iso_639_choices if item[1] == dict_info[1]]
+            country_code = "".join(country_code)
+            new_name = i.replace("munknown", country_code)
+            print("Moving {0} to {1}".format(i, new_name))
+            move(i, new_name)
